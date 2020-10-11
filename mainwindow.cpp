a65b11e1e6643a6bfb4a465f1f456d4b0d06b29d
Zablas <vagabaliukas@gmail.com>
2020-10-12 01:36:06 +0300

    Started improving the connection window

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,6 +2,10 @@
 #include "ui_mainwindow.h"
 #include <QSqlDatabase>
 #include <QSqlError>
+#include <QFile>
+#include <QDebug>
+#include <thread>
+#include <fstream>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -18,6 +22,7 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
+
     // Get the strings
     QString hostName = ui->inputHostName->text();
     QString dbName = ui->inputDbName->text();
@@ -25,16 +30,27 @@ void MainWindow::on_pushButton_clicked()
     QString password = ui->inputPassword->text();
     int port = ui->inputPort->text().toInt();
 
-    // Set up the connection
-    QSqlDatabase qdb = QSqlDatabase::addDatabase("QMYSQL");
-    qdb.setHostName(hostName);
-    qdb.setDatabaseName(dbName);
-    qdb.setUserName(username);
-    qdb.setPassword(password);
-    qdb.setPort(port);
+    // Thread separation
+    std::thread connThread([&]{
+        // Set up the connection
+        QSqlDatabase qdb = QSqlDatabase::addDatabase("QMYSQL");
+        qdb.setHostName(hostName);
+        qdb.setDatabaseName(dbName);
+        qdb.setUserName(username);
+        qdb.setPassword(password);
+        qdb.setPort(port);
 
-    // Check if it's open
-    bool isOpen = qdb.open();
-    if(isOpen) ui->label_5->setText("Connection success!");
-    else ui->label_5->setText(qdb.lastError().text());
+        // Check if it's open
+        bool isOpen = qdb.open();
+        if(isOpen) ui->label_5->setText("Connection success!");
+        else ui->label_5->setText(qdb.lastError().text());
+        if(ui->chkSaveInfo->isChecked())
+        {
+            QFile cachedDbFile("dblist.csv");
+            cachedDbFile.open(QFile::WriteOnly);
+            std::ofstream f("dblist.csv");
+            f << "LMAO\n";
+        }
+    });
+    connThread.join();
 }
